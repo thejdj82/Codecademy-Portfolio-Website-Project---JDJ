ccb55830ae75a878d38724371698062a56ca7c07
TheJDJ <13188402+thejdj82@users.noreply.github.com>
2022-12-12 21:08:08 -0600

    final upload of docs

--- index.js
+++ index.js
@@ -15,3 +15,21 @@ btn.addEventListener('click', function onClick(event) {
     // btn.style.color = 'blue';
   }
 });
+
+btn.addEventListener('click', function onClick(event) {
+  const backgroundColor = document.body.style.backgroundColor;
+
+  if (backgroundColor === 'white') {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    btn.style.backgroundColor = 'white';
+    btn.style.color = 'black';
+
+  } else {
+
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black"; 
+    btn.style.backgroundColor = 'black';
+    btn.style.color = 'white';
+  }
+});
